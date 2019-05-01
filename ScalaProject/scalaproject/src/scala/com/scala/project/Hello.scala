f454627655dd426c9db4c8eade365e49140be887
Manoj <manojgct84@gmail.com>
2019-05-01 20:41:08 +0530

    Learning scala

---
+++ ScalaProject/scalaproject/src/scala/com/scala/project/Hello.scala
@@ -0,0 +1,10 @@
+package scala.com.scala.project
+
+object Hello extends App {
+
+  def sayHiToScala(name: String): Unit = {
+    print("This is scala work ", name)
+  }
+
+  sayHiToScala("Scala")
+}
