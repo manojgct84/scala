be3b80758b8f7ac8ab8047980387a17295d0bd19
Manoj <manojgct84@gmail.com>
2019-05-04 11:41:26 +0530

    Todays learning

--- ScalaProject/scalaproject/src/main/scala/scala/project/ScalaSimple.scala
+++ ScalaProject/scalaproject/src/main/scala/scala/project/ScalaSimple.scala
@@ -1,8 +1,9 @@
-import org.apache.spark.sql.SparkSession
-//import org.apache.hadoop.mapreduce.v2.app.webapp.App
+import org.apache.spark.api.java.function.ForeachFunction
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types._
 
 /**
-	* Hello world!
+	* @Author : Manojkumar M
 	*
 	*/
 object ScalaSimple {
@@ -27,6 +28,42 @@ object ScalaSimple {
 
 		df.show()
 		dataFrameCSV.show()
-	}
 
+
+		println("******************This is a user defined schema for csv file******************************")
+
+		val structData = new StructType().add("transactionId", "String", true).add("customerId", IntegerType, true)
+			.add("itemId", IntegerType, true).add("amountPaid", FloatType, true)
+
+		val schemaDF = sparkSession.read.format("csv").option("header", "true")
+			.schema(structData).load("src/main/resources/sales.csv")
+
+		schemaDF.show()
+
+		//val peopleDF = sparkSession.read.json("examples/src/main/resources/people.json")
+		//Writting a file into parquet file
+		//schemaDF.select("transactionId", "customerId", "itemId", "amountPaid").write.save("src/main/resources/sales" +
+		//	".parquet")
+
+		//To write a parquet file
+		//schemaDF.write.parquet("src/main/resources/sales.parquet")
+
+		val sqlDF = sparkSession.read.parquet("src/main/resources/sales.parquet")
+		sqlDF.show()
+
+		//Creating a rdd in sparksession  in 2.x - This is because rdd is part of the spark context and dataFrame and
+		// dataset is part of the sparksession
+
+		val rddData = sparkSession.sparkContext.textFile("src/main/resources/data.txt")
+
+		val count = rddData.countByValue();
+
+		rddData.foreach(items => println("Print the value  %s".format(items)))
+
+		sqlDF.foreach(rows => println("This is data frame printing : %s".format(rows)))
+
+		println("The count by value using RDD %s".format(count))
+
+
+	}
 }
